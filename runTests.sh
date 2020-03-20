a030a20c36dba3eee0841ed6be1cc7a057eacab7
Nikita Lebedev <y87cgp@gmail.com>
2020-03-20 18:05:48 +0300

    tests fix

---
+++ runTests.sh
@@ -0,0 +1,7 @@
+curl -X POST -H "Content-Type: application/json" -d '{"name": "Первый канал 1"}' http://localhost:8000/channels
+echo
+echo
+curl -X POST -H "Content-Type: application/json" -d '{"answer_to_id": 10, "user_id": 1, "text": "Hello world!"}' http://localhost:8000/channels/0/messages
+echo
+echo
+curl http://localhost:8000/channels/0/messages
