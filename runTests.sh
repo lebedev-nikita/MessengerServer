de1b0a38efc99aa1e3a489461d83148c688c47e8
Nikita Lebedev <y87cgp@gmail.com>
2020-03-27 00:09:40 +0300

    improved tests

--- runTests.sh
+++ runTests.sh
@@ -1,13 +1,22 @@
-curl -X POST -H "Content-Type: application/json" -d '{"name": "Первый канал 1"}' http://localhost:8000/channels
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    SRV=localhost:8000
+else
+    SRV=my-messenger-server.herokuapp.com
+fi
+
+curl -X POST -H "Content-Type: application/json" -d '{"name": "Первый канал 1"}' http://$SRV/channels
 echo
 echo
-curl -X POST -H "Content-Type: application/json" -d '{"answer_to_id": 10, "user_id": 1, "text": "Hello world!"}' http://localhost:8000/channels/0/messages
+curl -X POST -H "Content-Type: application/json" -d '{"answer_to_id": 10, "user_id": 1, "text": "Hello world!"}' http://$SRV/channels/0/messages
 echo
 echo
-curl http://localhost:8000/channels/0/messages
+curl http://$SRV/channels/0/messages
 echo
 echo
-curl -X DELETE http://localhost:8000/clean
+curl -X DELETE http://$SRV/clean
 echo
 echo
-curl http://localhost:8000/channels/0/messages
+curl http://$SRV/channels/0/messages
